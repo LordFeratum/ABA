d2e31f7ecb3ca8045f2ef3c5a47817619486e915
Llorenç Segui Capllonch <llorensseguicapllonch@MacBook-Pro-de-Llorens.local>
2015-03-26 17:40:59 +0100

    Creacion de los modelos (Anuncio y Noticia)

--- aba/web/models.py
+++ aba/web/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
-# Create your models here.
+# Creamos los modelos. La base de datos constara de tres tabals sin relacion.
+
+class Anuncio (models.Model):
+	titulo = models.CharField(max_lenght=256)
+	texto = models.TextField()
+	fechaPublicacion = models.DateField()
+	nick = models.CharField(max_lenght=50)
+
+
+class Noticia (models.Model):
+	titulo = models.CharField(max_lenght=256)
+	texto = models.TextField()
+	fechaPublicacion = models.DateField()
